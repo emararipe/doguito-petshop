74a14dfc9ab91936077e42de0fe5b463c364e76e
emararipe <emararipe@gmail.com>
2023-04-22 16:45:13 -0300

    aplicação de  mensagens de erro estilizadas

--- js/validacao.js
+++ js/validacao.js
@@ -7,10 +7,53 @@ export function valida(input) {
 
   if (!input.validity.valid) {
     input.parentElement.classList.add("input-container--invalido")
+    input.parentElement.querySelector(".input-mensagem-erro").innerHTML = mostraMensagemErro(tipoInput, input)
   } else {
     input.parentElement.classList.remove("input-container--invalido")
+    input.parentElement.querySelector(".input-mensagem-erro").innerHTML = ""
   }
 }
+
+function mostraMensagemErro(tipoDeInput, input) {
+  let mensagem = ""
+  tiposDeErro.forEach(erro => {
+    if(input.validity[erro]){
+      mensagem = mensagensDeErro[tipoDeInput][erro]
+    }
+  }
+)
+  return mensagem
+
+}
+
+const tiposDeErro = [
+  "valueMissing",
+  "typeMismatch",
+  "patternMismatch",
+  "customError"
+]
+
+const mensagensDeErro = {
+  nome: {
+    valueMissing: "O campo nome não pode estar em vazio."
+  },
+
+  email: {
+    valueMissing: "O campo email não pode estar em vazio.",
+    patternMismatch: "Por favor, insira um endereço de e-mail válido. Ex.: user@domain.com, user@edu.br"
+  },
+
+  senha: {
+    valueMissing: "O campo senha não pode estar em vazio.",
+    patternMismatch: "A senha deve conter entre 6 e 12 caracteres, contendo pelo menos uma letra maiúscula, um número e não deve conter símbolos."
+  },
+
+  dataNascimento: {
+    valueMissing: "O campo da data de nascimento não pode estar em vazio.",
+    customError: "Você precisa ter mais de 18 anos para se cadastrar."
+  }
+}
+
 
 const validadores = {
   dataNascimento: input => validaNascimento(input), 
